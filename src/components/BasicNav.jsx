2ba34ad6341b8a8d5f9415648f62530915382f91
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-04-16 00:37:46 -0600

    add linkCode

--- src/components/BasicNav.jsx
+++ src/components/BasicNav.jsx
@@ -83,7 +83,7 @@ const BasicNav = ({ isOffcanvas, handleCloseOffcanvas }) => {
           <span>Github</span>
           </div>
         </NavDropdown.Item>
-        <NavDropdown.Item href="#">
+        <NavDropdown.Item href="https://github.com/ROR2022/PRODIGY_WD_01/">
           <div className="d-flex justify-content-start align-items-center">
           <FaCode style={{ width: "50px", marginLeft:'-10px', marginRight:'-5px' }} />
           <span>Code</span>
